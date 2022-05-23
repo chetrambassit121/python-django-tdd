cceb84f5b8c044ed29c0dda1c576b8bc36c63cbc
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-23 15:08:54 -0400

    initial commit

---
+++ lists/urls.py
@@ -0,0 +1,17 @@
+# from django.urls import path
+# from lists.views import home_page
+
+# urlpatterns = [
+
+# 	path("", home_page, name='home'),
+
+# ]
+
+
+
+# from django.conf.urls import url
+# from lists import views
+
+# urlpatterns = [
+#     url(r'^$lists_page/', views.lists_page, name='lists_page'),
+# ]
