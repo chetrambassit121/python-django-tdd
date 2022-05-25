4b868c91d87a797fd71e0fd349b20a84b56cae1a
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-25 11:33:37 -0400

    new list and add item functions with testing

--- lists/urls.py
+++ lists/urls.py
@@ -1,13 +1,56 @@
-from django.urls import path
-from lists.views import home_page, view_list
+from django.conf.urls import url
+from lists import views
 
 urlpatterns = [
-
-	path("", home_page, name='home'),
-	path("lists/the-only-list-in-the-world/", view_list, name='view_list'),
-
-
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
+
+
+
+
+
+
+
+
+
+
+# from django.urls import path
+# from lists.views import home_page, view_list, new_list, add_item
+
+# urlpatterns = [
+
+# 	path("new", new_list, name='new_list'),
+# 	path(r"^(\d+)/add_item$", add_item, name='add_item'),
+# 	path(r"^(\d+)/$", view_list, name='view_list'),
+
+
+# ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.urls import path
+# from lists.views import home_page, view_list
+
+# urlpatterns = [
+
+# 	path("", home_page, name='home'),
+# 	path("lists/the-only-list-in-the-world/", view_list, name='view_list'),
+
+
+# ]
 
 
 
