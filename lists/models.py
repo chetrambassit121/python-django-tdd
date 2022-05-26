3720e2dc515095e7bc28220321bb30878a0aafca
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-26 19:50:04 -0400

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,37 @@
 from django.db import models
+from django.shortcuts import redirect, render, reverse
+
 
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
+
+
+
 
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.db import models
+
+# class List(models.Model):
+#     pass
+
+# class Item(models.Model):
+#     text = models.TextField(default='')
+#     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
