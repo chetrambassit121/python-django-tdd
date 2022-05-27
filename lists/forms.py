201520b72fafaf828fa45d60185ec68b0b4849c4
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-26 22:03:45 -0400

    added forms in our testd and views

--- lists/forms.py
+++ lists/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 from lists.models import Item
 
-
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
@@ -19,6 +18,46 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django import forms
+
+# from lists.models import Item
+
+
+# EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+# class ItemForm(forms.models.ModelForm):
+
+#     class Meta:
+#         model = Item
+#         fields = ('text',)
+#         widgets = {
+#             'text': forms.fields.TextInput(attrs={
+#                 'placeholder': 'Enter a to-do item',
+#                 'class': 'form-control input-lg',
+#             }),
+#         }
+#         error_messages = {
+#             'text': {'required': EMPTY_ITEM_ERROR}
+#         }
 
 
 
