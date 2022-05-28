b48e8c466dd160c1fa1ac06b0a17a8eb20f031b0
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-28 15:56:56 -0400

    accounts views and testing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
-# from django.conf.urls import url
-# from accounts import views
+from django.conf.urls import url
+from accounts import views
 
-# urlpatterns = [
-#     url(r'^send_email$', views.send_login_email, name='send_login_email'),
-#     url(r'^login$', views.login, name='login'),
-#     url(r'^logout$', views.logout, name='logout'),
-# ]
+urlpatterns = [
+    url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,80 @@
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages
+
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def send_login_email(request):
+#     email = request.POST['email']
+#     # send_mail(
+#     #     'Your login link for Superlists',
+#     #     'body text tbc',
+#     #     'noreply@superlists',
+#     #     [email],
+#     # )
+#     return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.core.mail import send_mail
+# from django.shortcuts import redirect
+
+# def send_login_email(request):
+#     return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import uuid
 # import sys
 # from django.contrib.auth import authenticate
