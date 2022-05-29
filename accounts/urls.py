e9a9de64a981441b4474ddf52c1c7f9424005406
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-28 21:53:48 -0400

    testing logout in production

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
     url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
+    # url(r'^logout$', logout, name='logout'),
+
 ]
--- superlist/settings/base.py
+++ superlist/settings/base.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [                                                            
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
