d63d44f0f9fa3bf27c951db49923fd2a056d4a27
Chetram Bassit <chetrambassit121@gmail.com>
2022-05-28 22:05:18 -0400

    changing logout import to work in prod

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from accounts import views
 # from django.contrib.auth import logout
-from django.contrib.auth.views import logout
+# from django.contrib.auth.views import logout
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
-    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
+    # url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout$', LogoutView.as_view(), name='logout'),
     # url(r'^logout$', logout, name='logout'),
 
 ]
--- superlist/settings/base.py
+++ superlist/settings/base.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [                                                            
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
